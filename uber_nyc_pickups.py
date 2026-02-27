abeb3b849fedb45a663cb5516d5da4226da611a8
Carolyn Challoner <16659436+cchalloner@users.noreply.github.com>
2026-02-27 14:59:22 -0600

    Add checkbox to show bar chart of pickups by hour

--- uber_nyc_pickups.py
+++ uber_nyc_pickups.py
@@ -31,10 +31,11 @@ if st.checkbox('Show raw data'):
 
 st.subheader('Number of pickups by hour')
 
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-st.bar_chart(hist_values)
+if st.checkbox('Show bar chart'):
+    st.subheader('Number of pickups by hour')
+    hist_values = np.histogram(
+        data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+    st.bar_chart(hist_values)
 
 st.subheader('Map of all pickups')
 
