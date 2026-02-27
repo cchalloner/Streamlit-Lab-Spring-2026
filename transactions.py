f4cbcec42c2fb3b15abc7b695d4c91ad3a91616b
Carolyn Challoner <16659436+cchalloner@users.noreply.github.com>
2026-02-27 15:56:11 -0600

    Rename title and update data display section

--- transactions.py
+++ transactions.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC') 
+st.title('Transaction Analysis') 
 
 DATA_URL = ('/data/transactions.csv') 
 
@@ -26,10 +26,3 @@ data_load_state.text("Done! (using st.cache_data)")
 st.subheader('Raw data')
 
 st.write(data)
-
-st.subheader('Spending Amounts')
-
-hist_values = np.histogram(
-    data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
-
-st.bar_chart(hist_values)
